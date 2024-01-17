e1885aaa016f960d97db08d2a96f829d64711564
lecielpourlamer <88413718+lecielpourlamer@users.noreply.github.com>
2024-01-17 15:23:31 +0900

    first commit

---
+++ src/main/java/com/zerock/ex1/controller/SampleController.java
@@ -0,0 +1,14 @@
+package com.zerock.ex1.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+public class SampleController {
+
+    @GetMapping("/hello")
+    public String[] hello() {
+        return new String[] { "Hello", "World" };
+    }
+
+}
